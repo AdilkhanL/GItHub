40ff7f015c6d6cbcc30ce7ad07e42af3f0311ff5
AdilkhanL <a0462618@gmail.com>
2025-01-16 18:31:53 +0500

    third

---
+++ comments.py
@@ -0,0 +1,6 @@
+#this is a comment
+print("Hello, World!") #this is a comment
+#this is a comment
+"""
+this is a comment
+"""
--- syntax.py
+++ syntax.py
@@ -1,2 +1,2 @@
 if 5 > 2:
-    print("five is greater than two")
+    print("five is greater than two!")
