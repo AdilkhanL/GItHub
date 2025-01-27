9c0ac9fd617d7caac32a5d633e193451d4fd15f9
AdilkhanL <a0462618@gmail.com>
2025-01-27 10:07:14 +0500

    lab1 renamed

---
+++ Lab1/Types_and_random.py
@@ -0,0 +1,22 @@
+import random
+x = 5 #int
+print(type(x))
+
+y = True #bool
+print(type(y))
+
+z = ["banana", "apple", "lemon"] #list
+print(type(z))
+
+q = 2.1 #float
+print(type(q))
+
+w = 3.4e3 #float
+print(type(w))
+
+s = "How are you?" #str
+print(type(s))
+
+
+#Random
+print(random.randrange(1, 10))
---
+++ Lab1/string_3.py
@@ -0,0 +1,8 @@
+a = "We are the so-called \"Vikings\" from the north."
+print(a)
+
+b = 'Milk\'s color is \nWhite'
+print(b)
+
+txt = "\110\145\154\154\157"
+print(txt)
